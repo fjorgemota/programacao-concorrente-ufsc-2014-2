1e6efa1214bf57958b5cd6847afdff913878fb45
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-09-02 22:40:17 -0300

    Corrigido memory leaks com a ajuda do nosso amado valgrind <3 (esse foi o trabalho entregue)

--- Trabalho Pratico 1/trabalho.c
+++ Trabalho Pratico 1/trabalho.c
@@ -18,6 +18,9 @@ pthread_mutex_t mutex; // Variável global mutex
 int* geraVetor() {
 	int a;
 	int* resultado = (int*) malloc(sizeof(int)*VECSIZE);
+	if(resultado == NULL){
+		return NULL;
+	}
 	for (a=0; a < VECSIZE; a++) {
 		resultado[a] = rand()%10;
 	}
@@ -42,6 +45,8 @@ void *calculaProdutoEscalar(void *argumento){
 	pthread_mutex_lock(&mutex); //início da região crítica
 	sum += resultado;
 	pthread_mutex_unlock(&mutex); //Final da região crítica
+	free(argumentos->A);
+	free(argumentos->B);
 	pthread_exit(NULL);
 }
 
@@ -68,13 +73,11 @@ void imprimeResultado() {
 	printf("Produto Escalar = %d\n", sum);
 }
 
-void prepararThreads(int nThreads, int *A, int *B, pthread_t *thread) {
+void prepararThreads(int nThreads, int *A, int *B, pthread_t *thread, argumentosThread *argumentos) {
 	int contador, tamanhoVetorBase, tamanhoVetor, contadorVetor, contadorVetorArgumento, resto;
 	tamanhoVetorBase = VECSIZE/nThreads;
 	resto = VECSIZE%nThreads;
 	contadorVetor = 0;
-	argumentosThread *argumentos = (argumentosThread*)malloc(sizeof(argumentosThread)*NTHREADS);
-	pthread_mutex_init(&mutex, NULL); // criação da mutex
 	for (contador = 0; contador < nThreads; contador++) {
 		argumentos[contador].idThread = contador; // Numero de thread
 		argumentos[contador].posicaoInicial = contadorVetor; // Index inicial
@@ -84,7 +87,24 @@ void prepararThreads(int nThreads, int *A, int *B, pthread_t *thread) {
 			resto--;
 		}
 		argumentos[contador].A = (int*) malloc(sizeof(int)*tamanhoVetor);
+		if(argumentos[contador].A == NULL){
+			printf("Não foi possível alocar memória para o vetor que será passado para a thread\n");
+			free(A);
+			free(B);
+			free(thread);
+			free(argumentos);
+			pthread_exit(NULL);
+		}
 		argumentos[contador].B = (int*) malloc(sizeof(int)*tamanhoVetor);
+		if(argumentos[contador].B == NULL){
+			printf("Não foi possível alocar memória para o vetor que será passado para a thread\n");
+			free(A);
+			free(B);
+			free(thread);
+			free(argumentos[contador].A);
+			free(argumentos);
+			pthread_exit(NULL);
+		}
 		for(contadorVetorArgumento = 0; contadorVetorArgumento < tamanhoVetor; contadorVetorArgumento++){
 			argumentos[contador].A[contadorVetorArgumento] = A[contadorVetor];
 			argumentos[contador].B[contadorVetorArgumento] = B[contadorVetor];
@@ -93,33 +113,62 @@ void prepararThreads(int nThreads, int *A, int *B, pthread_t *thread) {
 		argumentos[contador].posicaoFinal = contadorVetor; // Index final
 		pthread_create(&thread[contador], NULL, calculaProdutoEscalar, (void *) &argumentos[contador]);
 	}
-	pthread_mutex_destroy(&mutex); //destruição da mutex
 }
 
 int calculaThreads(){
 	int divisao = VECSIZE/NTHREADS;
 	int resto = VECSIZE%NTHREADS;
-	if(divisao == 0) { 
+	if(divisao == 0) {
 		return resto;
 	} else {
 		return NTHREADS;
 	}
 }
 
-int main(int argc, char *argv) {
-        srand(time(NULL));
+int main(int argc, char **argv) {
+	srand(time(NULL));
 	int* A;
 	int* B;
 	A = geraVetor();
+	if(A == NULL){
+		printf("Não foi possível alocar memória para o vetor\n");
+		return 1;
+	}
+
 	B = geraVetor();
+	if(B == NULL){
+		printf("Não foi possível alocar memória para o vetor\n");
+		free(A);
+		return 1;
+	}
 
 	int nThreads = calculaThreads();
 	pthread_t *thread = (pthread_t*) malloc(sizeof(pthread_t)*nThreads);
+	if(thread == NULL){
+		printf("Erro: Não foi possivel alocar memória para as threads\n");
+		free(A);
+		free(B);
+		return 1;
+	}
+	argumentosThread *argumentos = (argumentosThread*)malloc(sizeof(argumentosThread)*NTHREADS);
+	if(argumentos == NULL){
+		printf("Erro: Não foi possivel alocar memória para os argumentos\n");
+		free(A);
+		free(B);
+		free(thread);
+		return 1;
+	}
+	pthread_mutex_init(&mutex, NULL); // criação da mutex
 
 	imprimirVetor('A', A);
 	imprimirVetor('B', B);
-	prepararThreads(nThreads, A, B, thread);
+	prepararThreads(nThreads, A, B, thread, argumentos);
 	esperarThreads(nThreads, thread);
 	imprimeResultado();
+	free(argumentos);
+	free(thread);
+	free(A);
+	free(B);
+	pthread_mutex_destroy(&mutex); //destruição da mutex
 	pthread_exit(NULL);
 }
