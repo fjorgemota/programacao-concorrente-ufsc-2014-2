0b1bb40432487fcec10b2fdc974adea92ab2480d
CaiqueMarques <caiquermarques95@gmail.com>
2014-11-02 22:44:41 +0000

    Arquivos das versoes paralela e sequencial documentadas e modularizadas. Atualizacao do arquivo make

--- Trabalho Pratico 3/versao-paralela.c
+++ Trabalho Pratico 3/versao-paralela.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <omp.h>
 
+/**!
+ * Calcula a soma dos divisores de um determinado número
+ * 
+ * @param numero: Número para o qual a soma dos divisores
+ *                deve ser calculado.
+ */ 
 int somaDivisores(int numero) {
     int divisores, contagem;
     divisores = 1 + numero;
-    // Estava esperando usar schedule(guided), porque, as primeiras
-    // threads iriam realizar mais contas, porém, "menores" (depende de
-    // qual foi o intervalo definido) e as últimas, com verificações maiores
-    // realizaria menos operações (gastando menos tempo para calcular).
-    // O que eu tenho dúvida é como seria o tamanho chunk (bloco).
     for (contagem = 2; contagem < numero; contagem++) {
         if (numero % contagem == 0) {
             divisores += contagem;
@@ -18,54 +19,118 @@ int somaDivisores(int numero) {
     return divisores;
 }
 
+/**!
+ * Calcula os números mutuamente amigos entre um determinado
+ * número mínimo e um determinado número máximo sem usar 
+ * nenhuma cache (calculando a soma dos divisores durante a 
+ * comparação)
+ * 
+ * @param minimo: Número mínimo para o qual o cache deve ser
+ *                pre-calculado
+ * @param maximo: Número máximo para o qual o cache deve ser
+ *                pre-calculado
+ * @param nThreads: Tamanho do chunk que deve ser distribuido
+ *                para cada thread durante o preenchimento do
+ *                cache.
+ */ 
+void calculaNumerosMutuamenteAmigosEntre(int minimo, int maximo, int nThreads) {
+    int n1, n2;
+    #pragma omp parallel
+    #pragma omp for schedule(dynamic, nThreads) private(n1, n2)
+    for(n1 = minimo; n1 <= maximo; n1++) {
+        double soma1 = (double) somaDivisores(n1);
+        double resposta1 = soma1/n1;
+        for(n2 = n1 + 1; n2 <= maximo; n2++) {
+            double soma2 = (double) somaDivisores(n2);
+            double resposta2 = soma2/n2;
+            if (resposta1 == resposta2) {
+                printf("%d e %d são mutuamente amigos\n", n1, n2);
+            }
+        }   
+    }
+}
+
+/**!
+ * Calcula os números mutuamente amigos entre um determinado
+ * número mínimo e um determinado número máximo usando a cache
+ * criada pela função criaCacheEntre.
+ * 
+ * @param minimo: Número mínimo para o qual o cache deve ser
+ *                pre-calculado
+ * @param maximo: Número máximo para o qual o cache deve ser
+ *                pre-calculado
+ * @param nThreads: Tamanho do chunk que deve ser distribuido
+ *                para cada thread durante o preenchimento do
+ *                cache.
+ * @param cache: Cache a ser usada como referência para o
+ *                calculo dos números mutuamente amigos.
+ */ 
+void calculaNumerosMutuamenteAmigosUsandoCacheEntre(int minimo, int maximo, int nThreads, double *cache) {
+    if (cache == NULL) {
+        calculaNumerosMutuamenteAmigosEntre(minimo, maximo, nThreads);
+        return;
+    }
+    int n1, n2, intervalo;
+    intervalo = maximo - minimo;
+    #pragma omp parallel
+    #pragma omp for schedule(dynamic, nThreads) private(n1, n2)
+    for(n1 = 0; n1 <= intervalo; n1++) {
+        double resposta1 = (cache[n1]/(n1+minimo));
+        for(n2 = n1 + 1; n2 <= intervalo; n2++) {
+            double resposta2 = (cache[n2]/(n2+minimo));
+            if (resposta1 == resposta2) {
+                printf("%d e %d são mutuamente amigos\n", n1 + minimo, n2 + minimo);
+            }
+        }   
+    }
+}
+
+/**!
+ * Cria um vetor de cache e o preenche com valores da soma 
+ * de divisores para todos os numeros entre um determinado
+ * número mínimo e um determinado número máximo e retorna
+ * o ponteiro para esse vetor.
+ * 
+ * @param minimo: Número mínimo para o qual o cache deve ser
+ *                pre-calculado
+ * @param maximo: Número máximo para o qual o cache deve ser
+ *                pre-calculado
+ * @param nThreads: Tamanho do chunk que deve ser distribuido
+ *                para cada thread durante o preenchimento do
+ *                cache.
+ */ 
+double* criaCacheEntre(int minimo, int maximo, int nThreads) {
+    int intervalo, n1;
+    double *cache;
+    intervalo = maximo-minimo;
+    cache = (double*) malloc((intervalo+1)*sizeof(double));
+    #pragma omp parallel
+    #pragma omp for schedule(dynamic, nThreads) private(n1)
+    for(n1 = 0; n1 <= intervalo; n1++) {
+        cache[n1] = (double) somaDivisores(n1 + minimo);
+    }
+    return cache;
+}
+
 int main(int argc, char **argv) {
     if (argc < 4) {
         printf("./versao-paralela <min> <max> <nthreads>\n");
         return 1;
     }
-    int n1, n2, minimo, maximo, nthreads, intervalo;
-    double *cache;
+    int minimo, maximo, nThreads;
     minimo = atoi(argv[1]);
     maximo = atoi(argv[2]);
-    intervalo = maximo-minimo;
-    nthreads = atoi(argv[3]);
-    if (nthreads < 1) {
+    nThreads = atoi(argv[3]);
+    if (nThreads < 1) {
         printf("nthreads deve ser maior ou igual a 1\n");
         return 1;
     }
-    omp_set_num_threads(nthreads);
-    cache = (double*) malloc(sizeof(double)*intervalo);
+    omp_set_num_threads(nThreads);
+    double *cache = criaCacheEntre(minimo, maximo, nThreads);
     if (cache == NULL) {
-        #pragma omp parallel
-        #pragma omp for schedule(dynamic, nthreads) private(n1, n2)
-        for(n1 = minimo; n1 <= maximo; n1++) {
-            double soma1 = (double) somaDivisores(n1);
-            double resposta1 = soma1/n1;
-            for(n2 = n1 + 1; n2 <= maximo; n2++) {
-                double soma2 = (double) somaDivisores(n2);
-                double resposta2 = soma2/n2;
-                if (resposta1 == resposta2) {
-                    printf("%d e %d são mutuamente amigos\n", n1, n2);
-                }
-            }   
-        }
+        calculaNumerosMutuamenteAmigosEntre(minimo, maximo, nThreads);
     } else {
-        #pragma omp parallel
-        #pragma omp for schedule(dynamic, nthreads) private(n1)
-        for(n1 = 0; n1 <= intervalo; n1++) {
-            cache[n1] = (double) somaDivisores(n1 + minimo);
-        }
-        #pragma omp parallel
-        #pragma omp for schedule(dynamic, nthreads) private(n1, n2)
-        for(n1 = 0; n1 <= intervalo; n1++) {
-            double resposta1 = (cache[n1]/(n1+minimo));
-            for(n2 = n1 + 1; n2 <= intervalo; n2++) {
-                double resposta2 = (cache[n2]/(n2+minimo));
-                if (resposta1 == resposta2) {
-                    printf("%d e %d são mutuamente amigos\n", n1 + minimo, n2 + minimo);
-                }
-            }   
-        }
+        calculaNumerosMutuamenteAmigosUsandoCacheEntre(minimo, maximo, nThreads, cache);
         free(cache);
     }
     
--- Trabalho Pratico 3/versao-sequencial.c
+++ Trabalho Pratico 3/versao-sequencial.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**!
+ * Calcula e soma todos os divisores de um determinado numero
+ *
+ * @param numero: O numero que tera seus divisores verificados 
+ *                e somados
+ */
 int somaDivisores(int numero) {
     int divisores, contagem;
     divisores = 1 + numero;
@@ -12,42 +18,88 @@ int somaDivisores(int numero) {
     return divisores;
 }
 
+/**!
+ * Configura um vetor (cache) com a soma dos divisores dos 
+ * numeros pertencentes ao intervalo de um numero minimo a
+ * um maximo, retornando o ponteiro deste valor
+ *
+ * @param minimo: O numero minimo do intervalo a ser
+ *                calculado para a cache
+ * @param maximo: O numero maximo do intervalo a ser
+ *                calculado para a cache
+ */
+double* configuraCache(int minimo, int maximo) {
+    int intervalo, n1;
+    double* cache;
+    intervalo = maximo - minimo;
+    cache = (double*) malloc((intervalo+1)*sizeof(double));
+    for(n1 = 0; n1 <= intervalo; n1++) {
+            cache[n1] = (double) somaDivisores(n1 + minimo);
+    }
+    return cache;
+}
+
+/**!
+ * Calcula quais os pares de numeros mutuamente amigos de um 
+ * dado intervalo sem o uso do vetor cache criado na funçao 
+ * configuraCache
+ *
+ * @param minimo: O numero minimo que compoe o extremo do intervalo
+ * @param maximo: O numero maximo que compoe o extermo do intervalo
+ */
+void calculaNumerosMutuamenteAmigosEntre(int minimo, int maximo) {
+    int n1, n2;
+    for(n1 = minimo; n1 <= maximo; n1++) {
+        double soma1 = (double) somaDivisores(n1);
+        double resposta1 = soma1/n1;
+        for(n2 = n1 + 1; n2 <= maximo; n2++) {
+            double soma2 = (double) somaDivisores(n2);
+            double resposta2 = soma2/n2;
+            if (resposta1 == resposta2) {
+                printf("%d e %d são mutuamente amigos\n", n1, n2);
+            }
+        }   
+    }
+}
+
+/**!
+ * Calcula quais os pares de numeros mutuamente amigos de um
+ * dado intervalo com o uso do vetor cache, criado na funçao
+ * configuraCache
+ *
+ * @param minimo: O numero minimo que compoe o extremo do intervalo
+ * @param maximo: O numero maximo que compoe o extremo do intervalo
+ * @param cache: A cache com a soma dos numeros do intervalo
+ *               composto pelos valores de minimo e maximo 
+ */
+void calculaNumerosMutuamenteAmigosUsandoCache(int minimo, int maximo, double* cache) {
+    int n1, n2, intervalo;
+    intervalo = maximo - minimo;
+    for(n1 = 0; n1 <= intervalo; n1++) {
+        double resposta1 = (cache[n1]/(n1+minimo));
+        for(n2 = n1 + 1; n2 <= intervalo; n2++) {
+            double resposta2 = (cache[n2]/(n2+minimo));
+            if (resposta1 == resposta2) {
+                printf("%d e %d são mutuamente amigos\n", n1 + minimo, n2 + minimo);
+            }
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc < 3) {
         printf("./versao-sequencial <min> <max>\n");
         return 1;
     }
     int n1, n2, minimo, maximo, intervalo;
-    double *cache;
     minimo = atoi(argv[1]);
     maximo = atoi(argv[2]);
     intervalo = maximo-minimo;
-    cache = (double*) malloc(sizeof(double)*intervalo);
+    double* cache = configuraCache(minimo, maximo);
     if (cache == NULL) {
-        for(n1 = minimo; n1 <= maximo; n1++) {
-            double soma1 = (double) somaDivisores(n1);
-            double resposta1 = soma1/n1;
-            for(n2 = n1 + 1; n2 <= maximo; n2++) {
-                double soma2 = (double) somaDivisores(n2);
-                double resposta2 = soma2/n2;
-                if (resposta1 == resposta2) {
-                    printf("%d e %d são mutuamente amigos\n", n1, n2);
-                }
-            }   
-        }
+        calculaNumerosMutuamenteAmigosEntre(minimo, maximo);
     } else {
-        for(n1 = 0; n1 <= intervalo; n1++) {
-            cache[n1] = (double) somaDivisores(n1 + minimo);
-        }
-        for(n1 = 0; n1 <= intervalo; n1++) {
-            double resposta1 = (cache[n1]/(n1+minimo));
-            for(n2 = n1 + 1; n2 <= intervalo; n2++) {
-                double resposta2 = (cache[n2]/(n2+minimo));
-                if (resposta1 == resposta2) {
-                    printf("%d e %d são mutuamente amigos\n", n1 + minimo, n2 + minimo);
-                }
-            }   
-        }
+        calculaNumerosMutuamenteAmigosUsandoCache(minimo, maximo, cache);
         free(cache);
     }
     return 0;
